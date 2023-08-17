6a362ca2cd2e10f2fa9a6d5f1be64979645933be
Siddharth Sahani <sidhucroedde@gmail.com>
2023-08-17 21:54:03 +0530

    Added new test scene

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <raylib.h>
 
 
-rt::Scene create_scene() {
+rt::Scene create_test_scene_1() {
     rt::Scene scene;
 
     // creating materials
@@ -45,6 +45,44 @@ rt::Scene create_scene() {
 }
 
 
+rt::Scene create_test_scene_2() {
+    rt::Scene scene;
+    // creating materials
+    {
+        rt::Material& material = scene.materials.emplace_back();
+        material.albedo = {0.2, 0.6, 1.0};
+    }
+    {
+        rt::Material& material = scene.materials.emplace_back();
+        material.albedo = {0.2, 0.6, 0.0};
+    }
+    // creating spheres
+    {
+        glm::vec3 position = {0.0, 0.0, 0.0};
+        float radius = 1.0;
+        int material_index = 0;
+        scene.spheres.emplace_back(new rt::Sphere(position, radius, material_index));
+    }
+    {
+        glm::vec3 position = {0.0, -101.0, 0.0};
+        float radius = 100.0;
+        int material_index = 1;
+        scene.spheres.emplace_back(new rt::Sphere(position, radius, material_index));
+    }
+
+    return scene;
+}
+
+
+int check_if_scene_changed() {
+    if (IsKeyDown(KEY_C)) {
+        if (IsKeyPressed(KEY_ONE)) { return 0; }
+        if (IsKeyPressed(KEY_TWO)) { return 1; }
+    }
+    return -1;
+}
+
+
 int main() {
     const int window_width = 1080;
     const int window_height = 720;
@@ -56,13 +94,19 @@ int main() {
     InitWindow(window_width, window_height, "Raytracing with c++");
     SetTargetFPS(30);
 
-    rt::Scene scene = create_scene();
+    int current_scene_index = 0;
+    rt::Scene test_scenes[] = {
+        create_test_scene_1(),
+        create_test_scene_2(),
+    };
+
+    rt::Scene test_scene = test_scenes[current_scene_index];
 
     glm::vec3 camera_position = {0, 0, 6};
     glm::vec3 camera_direction = {0, 0, -1};
     rt::Camera camera(45.0f, {image_width, image_height}, camera_position, camera_direction);
 
-    rt::Renderer renderer({image_width, image_height}, scene, camera);
+    rt::Renderer renderer({image_width, image_height}, test_scene, camera);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -76,14 +120,21 @@ int main() {
             renderer.reset_frame_index();
         }
 
+        int new_scene_index = check_if_scene_changed();
+        if (new_scene_index != -1 && current_scene_index != new_scene_index) {
+            current_scene_index = new_scene_index;
+            test_scene = test_scenes[current_scene_index];
+            renderer.reset_frame_index();
+        }
+
         if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyDown(KEY_D)) {
             // debug info
             DrawText("Debug stuff", 10, window_height-150, 20, RED);
             DrawText(TextFormat("  Window size : %d x %d", window_width, window_height), 10, window_height-120, 17, RED);
             DrawText(TextFormat("  Renderer size : %d x %d", image_width, image_height), 10, window_height-100, 17, RED);
             DrawText(TextFormat("  Scaling factor : %f", scale), 10, window_height-80, 17, RED);
-            DrawText(TextFormat("  Number of spheres : %d", scene.spheres.size()), 10, window_height-60, 17, RED);
-            DrawText(TextFormat("  Number of materials : %d", scene.materials.size()), 10, window_height-40, 17, RED);
+            DrawText(TextFormat("  Number of spheres : %d", test_scene.spheres.size()), 10, window_height-60, 17, RED);
+            DrawText(TextFormat("  Number of materials : %d", test_scene.materials.size()), 10, window_height-40, 17, RED);
         }
 
         DrawFPS(10, 10);
